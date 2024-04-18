50189c12922c6f61b7ee83c97c7a3319c3557f8f
beerxd0001 <166892344+beerxd0001@users.noreply.github.com>
2024-04-18 16:19:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,39 +34,39 @@ client.on('ready', async () => {
   setInterval(() => {
     const moment = require('moment');
     const created = moment().format('YYYY-MM-DD HH:mm:ss ');
-    const change = ['https://cdn.discordapp.com/attachments/1070765609559785583/1229121789376991262/4D557C8B-F244-4DE5-8795-313F0BA1B10F.gif?ex=662e87de&is=661c12de&hm=d6df66ef285f9a206e95acaa0ecb0d69a282468c27256724755b8b0c8c7fbc1e&']; //รูปใหญ่
+    const change = ['https://cdn.discordapp.com/attachments/1227927155732774995/1227927385152684093/6CAECF6A-DC7E-47F6-A2EF-A4BD2D48D4EE.gif?ex=662a2f7e&is=6617ba7e&hm=1c4c756420069e4eb68c9d99315055384e4724712270b1f9272cd05942209343&']; //รูปใหญ่
      //const poop = ['https://cdn.discordapp.com/attachments/1114551993499918438/1122462459710681170/1095697715238797403.gif','https://cdn.discordapp.com/attachments/1114551993499918438/1122462460817977395/1095697740543049819.gif','https://cdn.discordapp.com/attachments/1114551993499918438/1122462460125921341/1095697719189844048.gif','https://cdn.discordapp.com/attachments/1114551993499918438/1116029023957561425/1110570045454696509.gif']; // รูปเล็ก
      //const change2 = ['']; // ชื่อสลับ
-   // const yyyt = ['']; // ชื่อ button2
-    const ddds = ['https://discord.gg/mygv2']; // ลิ้ง button1
+    const yyyt = ['⪩ kungYuna']; // ชื่อ button2
+    const ddds = ['https://www.instagram.com/yoojiwuw_/?igsh=bHljdTAzMDd1ZzQ1&utm_source=qr']; // ลิ้ง button1
 
-    const iooi = ['💦  My Gang 🌊']; // ชื่อ button 1
-//  const iiio = ['https://discord.gg/mygv2']; // ลิ้ง button 2
-  const tyyy = yyyt[Math.floor(Math.random()*yyyt.length)]; // ชื่อ button 2
-  const sddd = ddds[Math.floor(Math.random()*ddds.length)]; // ลิ้ง button 2
-    const oooi = 
+   const iooi = ['Join server ᩍ']; // ชื่อ button 1
+   const iiio = ['https://discord.com/invite/jzpRQEb3GY']; // ลิ้ง button 2
+   const tyyy = yyyt[Math.floor(Math.random()*yyyt.length)]; // ชื่อ button 2
+   const sddd = ddds[Math.floor(Math.random()*ddds.length)]; // ลิ้ง button 2
+   const oooi = 
 iiio[Math.floor(Math.random()*iiio.length)]; // button 1 ใส่ลิ้ง
     const ioii =
 iooi[Math.floor(Math.random()*iooi.length)]; // button 1 ใส่ชื่อ
-    const popp =
-// poop[Math.floor(Math.random()*poop.length)]; // รูปเล็ก
+    //const popp =
+//poop[Math.floor(Math.random()*poop.length)]; // รูปเล็ก
     const ssss = 
 change[Math.floor(Math.random()*change.length)]; // รูปใหญ่
     //const dwada = change2[Math.floor(Math.random()*change2.length)]; // ชื่อสลับคำ
     const r = new Discord.RichPresence()
       .setApplicationId('1112701450150232085')
       .setType('STREAMING')
-      .setURL('https://youtu.be/TVjWnkFYOYE?si=QvkTi__02WxEroBi')
+      .setURL('https://www.youtube.com/watch?v=xRzbLQ_WKPs')
       .setState(`Discord`)
-      .setName(`YouTube`)
-      .setDetails(`เชื่อใจใน My G`)
+      .setName(`🎮`)
+      .setDetails(`っ My puppy  Dm 24/7 I love you 3000`)
       //.setAssetsSmallImage(`${popp}`) 
 .setAssetsLargeImage(`${ssss}`)
 .setAssetsLargeText(`🕐 ${getTime()}  ${moment().format(' 📅 DD/MM/YYYY ')}`)
 .setAssetsSmallText(`ping : ${Math.round(client.ws.ping)}`)
 .addButton(`${ioii}`,`${oooi}`)  
 
-     // .addButton(`${tyyy}`,`${sddd}`)
+      .addButton(`${tyyy}`,`${sddd}`)
 //.setStartTimestamp("1672545600" || Date.now())
 //.setEndTimestamp("1704077999" || Date.now())
     client.user.setActivity(r);
@@ -104,4 +104,3 @@ let options = {
   function getTime() {
     return (new Date()).toLocaleString([], options).split(" ")[1].replaceAll(",", "");
           }	
-
