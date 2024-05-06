52fc733baa41f299bbe3ecc0cab0b31751f81337
beerxd0001 <166892344+beerxd0001@users.noreply.github.com>
2024-05-06 16:17:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const moment = require('moment');
     const created = moment().format('YYYY-MM-DD HH:mm:ss ');
-    const change = ['https://cdn.discordapp.com/attachments/1070765609559785583/1235440668428730429/24be30e6aa12792e58f1af96c762679d.gif?ex=66346148&is=66330fc8&hm=538a00a24fc96e12c76669966de194e519cce54f1748b82a7063cd0e5edd45b2&']; //รูปใหญ่
+    const change = ['https://cdn.discordapp.com/attachments/1070765609559785583/1236175585470976102/B991492D-C00D-4554-9375-E309F63C01F1.gif?ex=6639b0ba&is=66385f3a&hm=40706310d81c180c1712cb3787230d26525bff798092074895194e80f41ae740&']; //รูปใหญ่
      //const poop = ['https://cdn.discordapp.com/attachments/1114551993499918438/1122462459710681170/1095697715238797403.gif','https://cdn.discordapp.com/attachments/1114551993499918438/1122462460817977395/1095697740543049819.gif','https://cdn.discordapp.com/attachments/1114551993499918438/1122462460125921341/1095697719189844048.gif','https://cdn.discordapp.com/attachments/1114551993499918438/1116029023957561425/1110570045454696509.gif']; // รูปเล็ก
      //const change2 = ['']; // ชื่อสลับ
   const yyyt = ['💦  My Gang 🌊']; // ชื่อ button2
